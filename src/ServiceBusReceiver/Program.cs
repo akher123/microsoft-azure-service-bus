160001ca29db859a88c9f78cdbb7cf481c80c9c3
AU <au@nineyard.com>
2026-09-24 16:24:50 +0600

    ServiceBusReceiver

---
+++ src/ServiceBusReceiver/Program.cs
@@ -0,0 +1,15 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "";
+string queueName = "";
+
+//Create Service bus client
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+// Create service Reciver 
+ServiceBusReceiver serviceBusReceiver=serviceBusClient.CreateReceiver(queueName);
+
+// Receive the message 
+
+ServiceBusReceivedMessage msg = await serviceBusReceiver.ReceiveMessageAsync();
+
+Console.WriteLine("Message has been recieved...");

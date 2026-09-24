ba4d2fe0f634d6ddf02b6491e100b4bdebc349fa
AU <au@nineyard.com>
2026-09-24 17:15:01 +0600

    Receive object in Azure service bus receiver

---
+++ src/ServiceBusObjectReceiver/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace ServiceBusObjectReceiver;
+
+internal class Employee
+{
+    public Employee(string frirstName, string lastName, int salary)
+    {
+        FirstName = frirstName;
+        LastName = lastName;
+        Salary = salary;
+    }
+    public string FirstName { get; }
+    public string LastName { get; }
+    public int Salary { get; }
+
+    public static Employee Create(string firstName, string lastName, int salary)
+    {
+        return new Employee(firstName, lastName, salary);
+    }
+}
---
+++ src/ServiceBusObjectReceiver/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using ServiceBusObjectReceiver;
+
+
+string connectionString = "";
+string queueName = "";
+
+// Create Service bus client
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+// Create service buss receiver
+
+ServiceBusReceiver serviceBusReceiver= serviceBusClient.CreateReceiver(queueName);
+
+// Try to read the mssage 
+
+var  msg= await serviceBusReceiver.ReceiveMessageAsync();
+var messageInJson= msg.Body.ToString();
+
+// Late convert the message into Employee object using Desirialisation
+
+Employee employee= JsonConvert.DeserializeObject<Employee>(messageInJson)!;
+
+Console.WriteLine("Message Has been received successfully");

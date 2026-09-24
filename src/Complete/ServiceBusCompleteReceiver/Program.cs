b3140bcedfcad6e93be4a7957953523553c88cad
AU <au@nineyard.com>
2026-09-24 17:38:00 +0600

    CompleteMessageAsync

---
+++ src/Complete/ServiceBusCompleteReceiver/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "";
+string queueName = "";
+
+// Create Service bus client
+
+ServiceBusClient serviceBusClient=new ServiceBusClient(connectionString);
+
+
+// Create Service Buse Reciver
+
+ServiceBusReceiver serviceBusReceiver=serviceBusClient.CreateReceiver(queueName);
+
+// receive the message 
+
+ServiceBusReceivedMessage msg =await serviceBusReceiver.ReceiveMessageAsync();
+
+// lets complete the mesage
+
+await serviceBusReceiver.CompleteMessageAsync(msg);
+
+Console.WriteLine("The Receive message has been completed");

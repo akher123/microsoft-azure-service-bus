0ba5fc607215c33f0a7490633b40b100dac5c20f
AU <au@nineyard.com>
2026-09-24 18:47:07 +0600

    Deadletter Queue

---
+++ src/DeadLetter/ServiceBusDeadLetterMessage/Program.cs
@@ -0,0 +1,25 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "";
+string queueName = "";
+
+//Create Service bus client
+
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+
+// Service bus receiver
+
+ServiceBusReceiver serviceBusReceiver = serviceBusClient.CreateReceiver(queueName);
+
+// Recive the message
+
+ServiceBusReceivedMessage msg = await serviceBusReceiver.ReceiveMessageAsync();
+
+// Lets deadletter the message
+await serviceBusReceiver.DeadLetterMessageAsync(msg);
+// Lets read message from deadletter queue
+
+var reciveFromDeadLetterQueue= serviceBusClient.CreateReceiver(queueName, new ServiceBusReceiverOptions() { SubQueue=SubQueue.DeadLetter });
+var msgObtainedFromDeadLetterQueue=await reciveFromDeadLetterQueue.ReceiveMessageAsync();
+
+Console.WriteLine("Mesagge operation completed");

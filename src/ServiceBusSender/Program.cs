e6058bcfde480e31cb25afce8022900a7e807de7
AU <au@nineyard.com>
2026-09-24 15:42:44 +0600

    Send message from Azure service buss snder

---
+++ src/ServiceBusSender/Program.cs
@@ -0,0 +1,14 @@
+﻿using Azure.Messaging.ServiceBus;
+string connectionString = "";
+string queueName = "";
+
+// Create service bus client
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+// create service bus sender
+ServiceBusSender serviceBusSender = serviceBusClient.CreateSender(queueName);
+// send message using service bus sender
+
+ServiceBusMessage serviceBusMessage = new ServiceBusMessage("Hello from C# visual studio...");
+
+await serviceBusSender.SendMessageAsync(serviceBusMessage) ;
+Console.WriteLine("Message has been sent");

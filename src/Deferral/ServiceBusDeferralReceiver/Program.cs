61e443fb412e7b8a70160cc6af6b52f18e87c9f8
AU <au@nineyard.com>
2026-09-24 18:22:26 +0600

    Defeered Message and recive

---
+++ src/Deferral/ServiceBusDeferralReceiver/Program.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "";
+string queueName = "";
+
+//Create Service bus client
+
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+
+// Service bus receiver
+
+ServiceBusReceiver serviceBusReceiver = serviceBusClient.CreateReceiver(queueName);
+
+// Recive the message
+
+ServiceBusReceivedMessage msg = await serviceBusReceiver.ReceiveMessageAsync();
+
+// Lets deferred the message
+await serviceBusReceiver.DeferMessageAsync(msg);
+// Lets read defeered message
+int sequancenumber = 16;
+var defeeredMessage= await serviceBusReceiver.ReceiveDeferredMessageAsync(sequancenumber);
+
+Console.WriteLine("Mesagge operation completed");

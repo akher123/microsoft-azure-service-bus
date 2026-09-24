e2438abbaf4ef601834891e055ca30c790a68c79
AU <au@nineyard.com>
2026-09-24 16:59:58 +0600

    Send Object in Azure service bus

---
+++ src/ServiceBusObjectSender/Program.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using ServiceBusObjectSender;
+using System.Text;
+
+Console.WriteLine("Hello, World!");
+string connectionString = "";
+string queueName = "";
+
+// Create Client with the connectionstring
+ServiceBusClient serviceBusClient = new ServiceBusClient(connectionString);
+// Create service bus Sender
+ServiceBusSender serviceBusSender=serviceBusClient.CreateSender(queueName);
+
+// Send the message using service bus sender
+Employee employee = Employee.Create("Md", "Akheruzzaman", 20000);
+var employeInJson= JsonConvert.SerializeObject(employee); 
+ServiceBusMessage message = new(Encoding.UTF8.GetBytes(employeInJson));
+
+await serviceBusSender.SendMessageAsync(message);
+
+Console.WriteLine("Message has been sent...");
+

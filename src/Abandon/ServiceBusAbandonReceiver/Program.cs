87615f13b56bea37b098ff9105e816c39d41669f
AU <au@nineyard.com>
2026-09-24 17:54:03 +0600

    implement Abandon message in Service Bus Queue From Visual Studio C#

---
+++ src/Abandon/ServiceBusAbandonReceiver/Program.cs
@@ -0,0 +1,21 @@
+﻿using Azure.Messaging.ServiceBus;
+
+string connectionString = "";
+string queueName = "";
+
+//Create Service bus client
+
+ServiceBusClient serviceBusClient=new ServiceBusClient(connectionString);
+
+// Service bus receiver
+
+ServiceBusReceiver serviceBusReceiver= serviceBusClient.CreateReceiver(queueName);
+
+// Recive the message
+
+ServiceBusReceivedMessage msg= await serviceBusReceiver.ReceiveMessageAsync();
+
+// Lets abandon the message
+await serviceBusReceiver.AbandonMessageAsync(msg);
+
+Console.WriteLine("Mesagge operation completed");
